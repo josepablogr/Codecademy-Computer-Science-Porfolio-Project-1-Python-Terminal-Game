fb5ab6c2824615eaba75b54251e9b5c17f478901
JP_Gramajo <josepablo.gramajo@icloud.com>
2023-07-27 20:17:21 -0600

    Greeting message and inputs

--- CS101_Portfolio_Project_1.py
+++ CS101_Portfolio_Project_1.py
@@ -2,3 +2,9 @@ from art import *
 
 #GAME TITLE
 tprint("World Cup Trivia")
+
+print("Welcome to the most exciting FIFA World Cup Trivia Game! There will be multiple-choice questions about the biggest sports event. The first player to answer 6 questions correctly wins the game. If there's a tie, a sudden death round takes off. Please enter the players' names.")
+
+
+p1_name = input("P1 Name: ")
+p2_name = input("P2 Name: ")
